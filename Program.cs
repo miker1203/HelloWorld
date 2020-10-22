6bc67d1728c7f5f1f8f4d7639be1f25620385079
Mike Richardson <g.email.michael.richardson@gmail.com>
2020-10-22 10:48:50 -0500

    Update on linux

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace HelloWorld
 {
+    class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub World!");
+            // Console.WriteLine("Hello GitHub World!");
+
+            // // Type your username and press enter
+            // Console.WriteLine("Enter username:");
+
+            // // Create a string variable and get user input from the keyboard and store it in the variable
+            // string userName = Console.ReadLine();
+            // Console.WriteLine("Enter your age:");
+            // int age = Convert.ToInt32(Console.ReadLine());
+
+            // // Print the value of the variable (userName), which will display the input value
+            // Console.WriteLine("Username is: " + userName);                    
+            // Console.WriteLine("Your age is: " + age);
+
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
         }
     }
 }
+
+
